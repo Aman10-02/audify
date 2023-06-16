f8f02438924cc65203d8606c3b18697c7a6c7956
Aman10-02 <a.agarwala@iitg.ac.in>
2023-06-16 12:50:46 +0530

    adding_firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD3NbomXkNvyRcDFvWiPRoIzdCyJ4LpI5A",
+  authDomain: "audify-2a5df.firebaseapp.com",
+  projectId: "audify-2a5df",
+  storageBucket: "audify-2a5df.appspot.com",
+  messagingSenderId: "962410092265",
+  appId: "1:962410092265:web:2aaf65afe368878ed54278"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
