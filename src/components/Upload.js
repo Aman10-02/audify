8045c0a45c6e26714c97887f601feaae5ce399db
Aman10-02 <a.agarwala@iitg.ac.in>
2023-06-22 18:49:08 +0530

    changes

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import app from "../firebase"
 import { getDownloadURL, getStorage, ref, uploadBytesResumable, deleteObject } from 'firebase/storage';
+import { getFirestore } from "firebase/firestore";
+import { collection, doc, setDoc } from "firebase/firestore"; 
 
 function Upload() {
 
@@ -41,11 +43,20 @@ function Upload() {
         });
         const data = await response.json();
         setVal(data);
+        const db = getFirestore(app);
+        const toUpload =
+        { 
+          createdOn : Date(),
+          updatedOn : Date(),
+          url : data.url
+        }
+        const fileRef = doc(db, "Files", "firstuser" );
         deleteObject(storageRef).then(() => {
           console.log("File deleted successfully")
         }).catch((error) => {
           console.log(error);
         });
+        await setDoc(fileRef, {filename : toUpload} ,{merge: true})
       }
     );
   };
@@ -58,7 +69,15 @@ function Upload() {
         <input type="submit" value="done" />
       </form>
       {val &&
+       
+       
+      <>
         <a href={val.url}  >click here to download audio</a>
+        <div style={{width : "50px", height:"50px", margin:"auto", backgroundColor:"yellow"}} onClick={() => {
+         const audio = new Audio(val.url);
+         audio.play();
+        }} >play</div>
+      </>
       }
     </div>
   )
