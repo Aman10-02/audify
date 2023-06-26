b79fe8c821758796d0d1456a076ee9a196ac21e1
ArindamDongre <arindamdongre2253@gmail.com>
2023-06-26 10:59:52 +0530

    new

--- src/components/Upload/Upload.js
+++ src/components/Upload/Upload.js
@@ -1,16 +1,21 @@
-import React, { useState } from 'react';
-import { app, auth } from "../../firebase"
-import { getDownloadURL, getStorage, ref, uploadBytesResumable, deleteObject } from 'firebase/storage';
+import React, { useState } from "react";
+import { app, auth } from "../../firebase";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  deleteObject,
+} from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
-import { doc, setDoc } from "firebase/firestore"; 
+import { doc, setDoc } from "firebase/firestore";
 import { useNavigate } from "react-router-dom";
-import moment from 'moment';
 
 function Upload() {
   const navigate = useNavigate();
   const [val, setVal] = useState("");
   const [file, setFile] = useState();
-  const [fileName, setFileName] = useState("")
+  const [fileName, setFileName] = useState("");
   const handleSubmit = async (event) => {
     event.preventDefault();
 
@@ -18,52 +23,54 @@ function Upload() {
     const storage = getStorage(app);
     const storageRef = ref(storage, tempFileName);
     const uploadTask = uploadBytesResumable(storageRef, file);
-    uploadTask.on('state_changed',
+    uploadTask.on(
+      "state_changed",
       (snapshot) => {
-        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        console.log('Upload is ' + progress + '% done');
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log("Upload is " + progress + "% done");
         switch (snapshot.state) {
-          case 'paused':
-            console.log('Upload is paused');
+          case "paused":
+            console.log("Upload is paused");
             break;
-          case 'running':
-            console.log('Upload is running');
+          case "running":
+            console.log("Upload is running");
             break;
           default:
         }
       },
-      (error) => {
-
-      },
+      (error) => {},
       async () => {
         const downloadURL = await getDownloadURL(uploadTask.snapshot.ref);
         const response = await fetch("http://localhost:5000/", {
           method: "POST",
           headers: {
-            'Content-Type' : 'application/json'
+            "Content-Type": "application/json",
           },
-          body: JSON.stringify({url : downloadURL })
+          body: JSON.stringify({ url: downloadURL }),
         });
         const data = await response.json();
         setVal(data);
-        console.log("first : ", data, JSON.stringify(data))
+        console.log("first : ", data, JSON.stringify(data));
         const db = getFirestore(app);
         const toUpload = {};
-        toUpload[fileName] =  { 
-                          createdOn : Date(),
-                          updatedOn : Date(),
-                          url : data.url,
-                          duration: data.duration
-                        }
-        console.log(toUpload)
-        const fileRef = doc(db, "Files", auth.currentUser.uid );
-        deleteObject(storageRef).then(() => {
-          console.log("File deleted successfully")
-        }).catch((error) => {
-          console.log(error);
-        });
-        await setDoc(fileRef, toUpload ,{merge: true})
-        navigate('/');
+        toUpload[fileName] = {
+          createdOn: Date(),
+          updatedOn: Date(),
+          url: data.url,
+          duration: data.duration,
+        };
+        console.log(toUpload);
+        const fileRef = doc(db, "Files", auth.currentUser.uid);
+        deleteObject(storageRef)
+          .then(() => {
+            console.log("File deleted successfully");
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+        await setDoc(fileRef, toUpload, { merge: true });
+        navigate("/");
       }
     );
   };
@@ -71,24 +78,41 @@ function Upload() {
   return (
     <div className="App">
       <form onSubmit={handleSubmit}>
-        <input type="text" placeholder='name of file' required onChange={ e => setFileName(e.target.value) } />
-        <input type="file" required onChange={e =>setFile(e.target.files[0])}/>
+        <input
+          type="text"
+          placeholder="name of file"
+          required
+          onChange={(e) => setFileName(e.target.value)}
+        />
+        <input
+          type="file"
+          required
+          onChange={(e) => setFile(e.target.files[0])}
+        />
         <input type="submit" value="done" />
       </form>
       <div>{fileName}</div>
-      {val &&
-       
-       
-      <>
-        <a href={val.url}  >click here to download audio</a>
-        <div style={{width : "50px", height:"50px", margin:"auto", backgroundColor:"yellow"}} onClick={() => {
-         const audio = new Audio(val.url);
-         audio.play();
-        }} >play</div>
-      </>
-      }
+      {val && (
+        <>
+          <a href={val.url}>click here to download audio</a>
+          <div
+            style={{
+              width: "50px",
+              height: "50px",
+              margin: "auto",
+              backgroundColor: "yellow",
+            }}
+            onClick={() => {
+              const audio = new Audio(val.url);
+              audio.play();
+            }}
+          >
+            play
+          </div>
+        </>
+      )}
     </div>
-  )
+  );
 }
 
-export default Upload
+export default Upload;
