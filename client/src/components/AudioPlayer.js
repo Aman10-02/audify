dd2633818f5b721ebc78a1203f8620f480caae33
ArindamDongre <arindamdongre2253@gmail.com>
2023-07-03 16:24:58 +0530

    des

--- client/src/components/AudioPlayer.js
+++ client/src/components/AudioPlayer.js
@@ -1,16 +1,16 @@
-import React, { useState } from 'react';
-import Swal from 'sweetalert2';
-import './Upload/Upload.css'
+import React, { useState } from "react";
+import Swal from "sweetalert2";
+import "./Upload/Upload.css";
 
 const AudioPlayer = ({ captions, currentTime, updateCaptions }) => {
-  
   const [editedCaptions, setEditedCaptions] = useState(captions); // State to hold the edited captions
   const [isEditing, setIsEditing] = useState(false);
 
   // Filter captions based on current time
   const currentCaption = editedCaptions.find(
     (caption) =>
-      caption.startTime / 1000 <= currentTime && caption.endTime / 1000 > currentTime
+      caption.startTime / 1000 <= currentTime &&
+      caption.endTime / 1000 > currentTime
   );
 
   // Function to handle caption text change
@@ -18,51 +18,62 @@ const AudioPlayer = ({ captions, currentTime, updateCaptions }) => {
     const updatedCaptionText = e.target.value;
     setEditedCaptions((prevCaptions) =>
       prevCaptions.map((caption) =>
-        caption === currentCaption ? { ...caption, text: updatedCaptionText } : caption
+        caption === currentCaption
+          ? { ...caption, text: updatedCaptionText }
+          : caption
       )
     );
   };
   const handleEdit = async () => {
-    if(isEditing){ //save btn clicked
-        await Swal.fire({
-        title: 'Save Changes',
-        text: 'Are you sure you want to change the caption?',
-        icon: 'question',
+    if (isEditing) {
+      //save btn clicked
+      await Swal.fire({
+        title: "Save Changes",
+        text: "Are you sure you want to change the caption?",
+        icon: "question",
         showCancelButton: true,
-        confirmButtonColor: '#d33',
-        cancelButtonColor: '#3085d6',
-        confirmButtonText: 'Save',
-        cancelButtonText: 'Cancel',
+        confirmButtonColor: "#d33",
+        cancelButtonColor: "#3085d6",
+        confirmButtonText: "Save",
+        cancelButtonText: "Cancel",
         showLoaderOnConfirm: true,
-        preConfirm: async() => {
+        preConfirm: async () => {
           try {
-            const cancleBtn = Swal.getCancelButton()
-            cancleBtn.style.display = "none"
-            await updateCaptions(editedCaptions);         
+            const cancleBtn = Swal.getCancelButton();
+            cancleBtn.style.display = "none";
+            await updateCaptions(editedCaptions);
           } catch (error) {
-            Swal.fire('Error', 'An error occurred while saving the changes.', 'error');
+            Swal.fire(
+              "Error",
+              "An error occurred while saving the changes.",
+              "error"
+            );
           }
-        }
+        },
       });
-    };
-    setIsEditing(!isEditing)
+    }
+    setIsEditing(!isEditing);
   };
 
   return (
     <>
       <div className="caption">
-        <textarea className="caption-textarea" value={currentCaption ? currentCaption.text : ""} disabled = {!isEditing} onChange={handleCaptionChange} />
+        <textarea
+          className="caption-textarea"
+          value={currentCaption ? currentCaption.text : ""}
+          disabled={!isEditing}
+          onChange={handleCaptionChange}
+        />
       </div>
-      <button className='audio-player-button' onClick={handleEdit} > {isEditing ? "save" : "edit"} </button>
+      <button className="audio-player-button" onClick={handleEdit}>
+        {" "}
+        {isEditing ? "Save" : "Edit"}{" "}
+      </button>
     </>
   );
 };
 
 export default AudioPlayer;
-
-
-
-
 
 // import React, { useState, useEffect } from 'react';
 // // import { createFFmpeg } from '@ffmpeg/ffmpeg';
@@ -71,7 +82,6 @@ export default AudioPlayer;
 // import axios from 'axios'
 // // import file from "./files/aman.srt"
 // import srtParser2 from "srt-parser-2";
-
 
 // // import './AudioPlayer.css'; // Add custom CSS for styling
 
@@ -84,7 +94,6 @@ export default AudioPlayer;
 //   // var srt_array = parser.fromSrt();
 //   // console.log(srt_array);
 
-  
 //   // const [currentCaption, setCurrentCaption] = useState();
 //   // const [curTime, setCurTime] = useState();
 
@@ -93,21 +102,20 @@ export default AudioPlayer;
 //    captions.filter((caption) => caption.startTime/1000 <= currentTime && caption.endTime/1000 > currentTime)
 //   // }, []);
 
-
 //   return (
 //     <div className="audio-player">
 //       {
 //         currentCaption &&
-//         currentCaption.map( (caption, index) => 
+//         currentCaption.map( (caption, index) =>
 //           <div className="caption">"this is caption" {caption.text}</div>
 //          )
-        
+
 //       }
 //       {/* <div className="controls">
 //         Add audio controls (play, pause, etc.)
 //       </div>
 //       <div className="captions">
-        
+
 //           <div
 //             onClick={() =>
 //               playAudioWithCaption()
--- client/src/components/View/View.js
+++ client/src/components/View/View.js
@@ -1,95 +1,119 @@
-import React, { useEffect, useState } from 'react'
-import { useLocation } from 'react-router-dom'
-import AudioPlayer from '../AudioPlayer';
-import ReactAudioPlayer from 'react-audio-player';
-import { doc, getDoc, getFirestore, updateDoc } from 'firebase/firestore';
-import { app } from '../../firebase';
-import { getAuth } from 'firebase/auth';
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import AudioPlayer from "../AudioPlayer";
+import ReactAudioPlayer from "react-audio-player";
+import { doc, getDoc, getFirestore, updateDoc } from "firebase/firestore";
+import { app } from "../../firebase";
+import { getAuth } from "firebase/auth";
 
-import '../Upload/Upload.css'
-import Comments from '../Comments';
+import "../Upload/Upload.css";
+import Comments from "../Comments";
 
-
-// import './View.css';
+import "./View.css";
 
 function View() {
-    let { state } = useLocation();
-    const [currentTime, setCurrentTime] = useState();
-    const [captions, setCaptions] = useState();
-    const [comments, setComments] = useState([]);
-    const [aud, setAud] = useState();
-    const db = getFirestore(app);
-    const auth = getAuth();
-    const user = auth.currentUser.uid;
-    const docref = doc(db, "Files", user);
-    const updateCaptions = async (captions) => {
-        await updateDoc(docref, {
-                [state.fileName] : {
-                    ...aud,
-                    captions : captions,
-                    updatedOn: Date()
-                }
-        })
-        setCaptions(captions);
-    };
-    const updateComments = async (comments) => {
-        await updateDoc(docref, {
-                [state.fileName] : {
-                    ...aud,
-                    comments : comments,
-                    updatedOn: Date()
-                }
-        })
-        setComments(comments);
-    };
-    const seekToTimestamp = (timestamp) => {
-        const audio = document.getElementById("toChange");
-        audio.currentTime = convertTimestampToSeconds(timestamp);
-        audio.play();
-      };
-    
-      const convertTimestampToSeconds = (timestamp) => {
-        // Implement the conversion logic based on the timestamp format you're using
-        // Example: "00:01:30" => 90 seconds
-        const [hours, minutes, seconds] = timestamp.split(':');
-        return parseInt(hours) * 3600 + parseInt(minutes) * 60 + parseInt(seconds);
-      };
-    useEffect(() => {
-        const fetchData = async () => {
-            const dataref = await getDoc(docref);
-            const auds = dataref.data()
-            console.log(auds[state.fileName])
-            setAud(auds[state.fileName])
-            setCaptions(auds[state.fileName].captions)
-            setComments(auds[state.fileName].comments)
-        };
-        fetchData();
-        // Update the current time based on the video player's time        
-        const handleTimeUpdate = (event) => {
-            setCurrentTime(Math.floor(event.target.currentTime));
-        };
+  let { state } = useLocation();
+  const [currentTime, setCurrentTime] = useState();
+  const [captions, setCaptions] = useState();
+  const [comments, setComments] = useState([]);
+  const [aud, setAud] = useState();
+  const db = getFirestore(app);
+  const auth = getAuth();
+  const user = auth.currentUser.uid;
+  const docref = doc(db, "Files", user);
+  const updateCaptions = async (captions) => {
+    await updateDoc(docref, {
+      [state.fileName]: {
+        ...aud,
+        captions: captions,
+        updatedOn: Date(),
+      },
+    });
+    setCaptions(captions);
+  };
+  const updateComments = async (comments) => {
+    await updateDoc(docref, {
+      [state.fileName]: {
+        ...aud,
+        comments: comments,
+        updatedOn: Date(),
+      },
+    });
+    setComments(comments);
+  };
+  const seekToTimestamp = (timestamp) => {
+    const audio = document.getElementById("toChange");
+    audio.currentTime = convertTimestampToSeconds(timestamp);
+    audio.play();
+  };
 
-        const audioElement = document.getElementById("toChange");
-        console.log(audioElement)
-        audioElement.addEventListener('timeupdate', handleTimeUpdate);
+  const convertTimestampToSeconds = (timestamp) => {
+    // Implement the conversion logic based on the timestamp format you're using
+    // Example: "00:01:30" => 90 seconds
+    const [hours, minutes, seconds] = timestamp.split(":");
+    return parseInt(hours) * 3600 + parseInt(minutes) * 60 + parseInt(seconds);
+  };
+  useEffect(() => {
+    const fetchData = async () => {
+      const dataref = await getDoc(docref);
+      const auds = dataref.data();
+      console.log(auds[state.fileName]);
+      setAud(auds[state.fileName]);
+      setCaptions(auds[state.fileName].captions);
+      setComments(auds[state.fileName].comments);
+    };
+    fetchData();
+    // Update the current time based on the video player's time
+    const handleTimeUpdate = (event) => {
+      setCurrentTime(Math.floor(event.target.currentTime));
+    };
 
-        return () => {
-            // Clean up the event listener
-            audioElement.removeEventListener('timeupdate', handleTimeUpdate);
-        };
-    }, []);
-    return (
-        <div className='view-page-con' >
-            <div className='react-audio-player'>
-               <ReactAudioPlayer id='toChange' className='react-audio' src={state.url} controls />
-            </div>
-         
-            {captions &&
-                      <AudioPlayer captions={captions} currentTime={currentTime} updateCaptions={updateCaptions} />}
-            {/* <div className="caption">"this is caption" {console.log(currentCaption)}</div> */}
-            <Comments currentTime={currentTime} updateComments={updateComments} comments={comments} seekToTimestamp={seekToTimestamp} />
+    const audioElement = document.getElementById("toChange");
+    console.log(audioElement);
+    audioElement.addEventListener("timeupdate", handleTimeUpdate);
+
+    return () => {
+      // Clean up the event listener
+      audioElement.removeEventListener("timeupdate", handleTimeUpdate);
+    };
+  }, []);
+  return (
+    <div className="view-page-con">
+      <div className="react-audio-player">
+        <ReactAudioPlayer
+          id="toChange"
+          className="react-audio"
+          src={state.url}
+          controls
+        />
+      </div>
+      <div className="use">
+        <div className="capt">
+          {captions && (
+            <AudioPlayer
+              captions={captions}
+              currentTime={currentTime}
+              updateCaptions={updateCaptions}
+            />
+          )}
         </div>
-    )
+        <div className="cont">
+          <div className="cmnts">
+            <Comments
+              updateComments={updateComments}
+              currentTime={currentTime}
+              comments={comments}
+              seekToTimestamp={seekToTimestamp}
+              showAddCommentButton={false}
+            />
+          </div>
+          <button className="add-comments-button" onClick={updateComments}>
+            Add Comments
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default View
+export default View;
